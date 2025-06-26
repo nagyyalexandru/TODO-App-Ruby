401f93aa1ef57edb81404044f41f96d1a07fb966
Alexandru Nagy <nagy.alexandru@gmail.com>
2025-06-26 14:01:07 +0300

    fixing a recaptcha bug where blank response was sent

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-// import "https://www.google.com/recaptcha/api.js"
 import "controllers"
